0d595a3d2348d8d8ae11657bde78504ce4757d8f
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2025-11-24 20:57:00 -0300

    Add firebase config

---
+++ src/scripts/config.js
@@ -0,0 +1,16 @@
+const config = {
+    firebaseConfig: {
+        apiKey: "AIzaSyDeuH-r3C0sUKc6LFemH891TvySR90myP0",
+        authDomain: "transito-9-de-julio.firebaseapp.com",
+        projectId: "transito-9-de-julio",
+        storageBucket: "transito-9-de-julio.firebasestorage.app",
+        messagingSenderId: "1053463290562",
+        appId: "1:1053463290562:web:53ef7eacef393b5545915b",
+        measurementId: "G-F4GVKYPCB7"
+    },
+    appId: "transito-9-de-julio"
+};
+
+window.__firebase_config = JSON.stringify(config.firebaseConfig);
+window.__app_id = config.appId;
+window.__initial_auth_token = null;
